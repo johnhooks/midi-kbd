27bdc5551b92b9748a0dd5b320158dabbd5c445d
John Hooks <bitmachina@outlook.com>
2023-01-09 13:04:34 -0800

    docs(example): add base to vite config

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -3,6 +3,7 @@ import { fileURLToPath } from "node:url";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+	base: "/midi-kbd/",
 	resolve: {
 		alias: {
 			"midi-kbd": fileURLToPath(new URL("../src/index.ts", import.meta.url).href),
