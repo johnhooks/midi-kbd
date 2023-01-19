5827dffbeb3e08ff82492bcba59b3d306e81eac3
John Hooks <bitmachina@outlook.com>
2023-01-19 14:40:21 -0800

    build: call yarn exec in scripts

--- scripts/build-docs.js
+++ scripts/build-docs.js
@@ -8,7 +8,6 @@ import { exec } from "./utils/exec.js";
 
 const rootDir = path.resolve(fileURLToPath(new URL(".", import.meta.url)), "../");
 
-const apiDocumenterPath = path.join(rootDir, "node_modules/.bin/api-documenter");
 const apiJsonPath = path.join(rootDir, "temp/midi-kbd.api.json");
 
 if (!fs.existsSync(apiJsonPath)) {
@@ -25,7 +24,7 @@ const apiDocumenterArgs = ["markdown", "-i", "./temp", "-o", "./docs", ...args];
 
 console.log(`api-documenter ${apiDocumenterArgs.join(" ")}`);
 
-exec(apiDocumenterPath, apiDocumenterArgs)
+exec("yarn", ["exec", "api-documenter", ...apiDocumenterArgs])
 	.then(() => process.exit(0))
 	.catch((code) => {
 		process.exit(code);
