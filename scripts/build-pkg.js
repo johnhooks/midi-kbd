fb244b189d4c7ccf702059b79e88d04169216631
John Hooks <bitmachina@outlook.com>
2023-01-19 20:20:18 -0800

    chore: remove unused var from build-pkg script

--- scripts/build-pkg.js
+++ scripts/build-pkg.js
@@ -1,11 +1,6 @@
 #!/usr/bin/env node
 
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-
 import { exec } from "./utils/exec.js";
-
-const rootDir = path.resolve(fileURLToPath(new URL(".", import.meta.url)), "../");
 
 if (!process.env.CI) {
 	await exec("yarn", ["clean:all"]);
